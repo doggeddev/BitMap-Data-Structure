19a2c5600f5819c007ae5559c537feebd486aaff
rjdrexel <richdrexel@gmail.com>
2017-01-19 00:16:27 -0500

    updated solution

--- ProgrammingPearlsBook/Program.cs
+++ ProgrammingPearlsBook/Program.cs
@@ -10,6 +10,35 @@ namespace ProgrammingPearlsBook
     {
         static void Main(string[] args)
         {
+            //Bitmap for sorting a set of integers
+
+            int[] numbers = { 12345, 54637, 15478, 34521, 97624, 34516, 91452, 37815, 74863, 34578};
+
+            Console.WriteLine("writing 0s to bitmap");
+
+            int[] bitmap = new int[99999];
+
+            for(int i = 0; i < bitmap.Length; i++) //init all indicies to 0
+            {
+                bitmap[i] = 0;
+            }
+
+            Console.WriteLine("writing 1s to bitmap");
+
+            for (int i = 0; i < numbers.Length; i++) //for every integer read, mark a 1 in corresponding position
+            {
+                bitmap[numbers[i]] = 1;
+            }
+
+            for(int i = 0; i < bitmap.Length; i++) //every 1 in the array, write the index on the screen
+            {
+                if(bitmap[i] == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
